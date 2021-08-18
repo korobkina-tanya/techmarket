77a26212f1e2775cc1b0ce52695de7128df09529
tan4ik <kor1988tanya@gmail.com>
2021-08-18 23:57:30 +0300

    used the library - swiper

--- js/index.js
+++ js/index.js
@@ -126,3 +126,22 @@ window.addEventListener("keydown", function(evt) {
 
 
 // */
+
+//-----------------------------------------------------
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    //type: "progressbar"
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
